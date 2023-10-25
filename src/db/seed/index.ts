2828a3100ef90390fd9fc0eeff9525695433fed1
Pedro Gardim <me@pedrogardim.com>
2023-10-25 20:35:23 +0200

    feat: create seed tattoo works script

--- src/db/seed/index.ts
+++ src/db/seed/index.ts
@@ -1,6 +1,7 @@
 import { AppDataSource } from "..";
 import { seedUsers } from "./userSeeds";
-import { seedAppointments } from "./seedAppointments";
+import { seedAppointments } from "./appointmentsSeeds";
+import { seedTattooWorks } from "./tattooWorksSeeds";
 
 AppDataSource.initialize().then(async () => {
   console.log("Start seeding...");
@@ -9,6 +10,8 @@ AppDataSource.initialize().then(async () => {
   console.log("Users done");
   await seedAppointments(users);
   console.log("Appointments done");
+  await seedTattooWorks(users);
+  console.log("Tattoo works done");
   await AppDataSource.query("SET FOREIGN_KEY_CHECKS = 1;");
   AppDataSource.destroy();
 });
---
+++ src/db/seed/tattooWorksSeeds.ts
@@ -0,0 +1,26 @@
+import { Appointment, AppointmentType } from "../../models/Appointment";
+import { User } from "../../models/User";
+import { TattooWork } from "../../models/TattoWork";
+import { fakerES as faker } from "@faker-js/faker";
+
+export const seedTattooWorks = async (users: User[]) => {
+  const randomTypes = ["tattoo", "piercing"] as AppointmentType[];
+
+  const tattooists = users.filter((u) => u.role === "tattooist");
+
+  await TattooWork.clear();
+
+  const appointments = Array(300)
+    .fill(null)
+    .map((_, i) => {
+      const workType = faker.helpers.arrayElement(randomTypes);
+      return TattooWork.create({
+        description: faker.lorem.text().slice(0, 256),
+        image_url: `https://source.unsplash.com/featured/?${workType}-${i}`,
+        tattooist: faker.helpers.arrayElement(tattooists),
+        type: workType,
+      });
+    });
+
+  await TattooWork.save(appointments);
+};
