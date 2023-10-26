f051a44fc2fc114b95d70c9d16a339b4d1995000
Pedro Gardim <me@pedrogardim.com>
2023-10-26 20:55:48 +0200

    feat: add password encryptation, removed trycatch

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,23 +1,24 @@
+import bcrypt from "bcrypt";
 import { Handler } from "express";
 import { validateRegistrationData } from "../utils/validation";
 import { RegisterPayload } from "../types/auth";
+import { User } from "../models/User";
+
+//errors are handled by async wrapper and error middleware!
 
 export const register: Handler = async (req, res) => {
-  try {
-    const isValid = validateRegistrationData(req.body as RegisterPayload);
+  validateRegistrationData(req.body as RegisterPayload);
 
-    res.json(isValid);
-  } catch (err: any) {
-    const { message, code = 500 } = err;
-    res.status(code).json({ error: { code, message } });
-  }
+  const encryptedPassword = await bcrypt.hash(req.body.password, 10);
+
+  const user = await User.create({
+    ...req.body,
+    password: encryptedPassword,
+  }).save();
+
+  res.json(user);
 };
 
 export const login: Handler = async (req, res) => {
-  try {
-    res.json("Hello world");
-  } catch (err: any) {
-    const { message, code = 500 } = err;
-    res.status(code).json({ error: { code, message } });
-  }
+  res.json("Hello world");
 };
