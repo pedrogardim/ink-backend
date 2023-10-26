ceb37c98362f71dabb0bbd207912772127f44046
Pedro Gardim <me@pedrogardim.com>
2023-10-26 21:58:29 +0200

    feat: add jwt and formated response on register controller

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,10 +1,14 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { Handler } from "express";
 import { validateRegistrationData } from "../utils/validation";
 import { RegisterPayload } from "../types/auth";
 import { User } from "../models/User";
+import { getBaseUrl } from "../utils/format";
 
 //errors are handled by async wrapper and error middleware!
+
+const { JWT_SECRET = "" } = process.env;
 
 export const register: Handler = async (req, res) => {
   validateRegistrationData(req.body as RegisterPayload);
@@ -16,7 +20,25 @@ export const register: Handler = async (req, res) => {
     password: encryptedPassword,
   }).save();
 
-  res.json(user);
+  const token = jwt.sign({ id: user.id, role: user.role }, JWT_SECRET, {
+    expiresIn: "1h",
+  });
+
+  delete user.password;
+
+  res.json({
+    data: {
+      kind: "authentication",
+      token,
+      tokenIat: new Date(),
+      tokenExp: new Date(Date.now() + 1000 * 60 * 60),
+      user: {
+        kind: "user",
+        self: `${getBaseUrl(req)}/users/${user.id}`,
+        ...user,
+      },
+    },
+  });
 };
 
 export const login: Handler = async (req, res) => {
