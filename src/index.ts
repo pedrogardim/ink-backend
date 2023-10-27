707e510721a17c50fced3231bfcc86451bb006bb
Pedro Gardim <me@pedrogardim.com>
2023-10-27 14:30:30 +0200

    feat: add morgan logger middleware

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,12 @@ import docsRouter from "./routes/docsRoutes";
 
 import { errorHandler } from "./middleware/errorHandler";
 import { auth } from "./middleware/auth";
+import morgan from "morgan";
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use("/api/auth", authRouter);
 app.use("/docs", docsRouter);
