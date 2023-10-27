40bf638993ee40f3771e8e4854dcf35a2ce4ea41
Pedro Gardim <me@pedrogardim.com>
2023-10-27 19:17:37 +0200

    feat: add get user crud controllers

--- src/controllers/usersControllers.ts
+++ src/controllers/usersControllers.ts
@@ -1,14 +1,36 @@
 import { Handler } from "express";
 import { User } from "../models/User";
+import { formatPaginationResponse, formatUser } from "../utils/format";
 
 //Admin CRUD
 export const getUserById: Handler = async (req, res) => {
   const user = await User.findOneBy({ id: parseInt(req.params.id) });
-  res.status(200).json(user);
+  if (!user) {
+    throw { code: 404, message: "User not found" };
+  }
+  res.status(200).json({ data: formatUser(user, req) });
 };
 
 export const getUsers: Handler = async (req, res) => {
-  res.status(200).json("ok");
+  let { pageSize = 10, page = 1 } = req.query;
+  pageSize = parseInt(pageSize as string);
+  page = parseInt(page as string);
+
+  const [users, totalItems] = await User.findAndCount({
+    where: req.body,
+    take: pageSize,
+    skip: (page - 1) * pageSize,
+  });
+
+  res.status(200).json(
+    formatPaginationResponse({
+      req,
+      page,
+      pageSize,
+      totalItems,
+      items: users.map((user) => formatUser(user, req)),
+    })
+  );
 };
 
 export const createUser: Handler = async (req, res) => {
