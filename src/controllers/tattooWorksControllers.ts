f581621e5cf552dcd0b5b125b19cbc935986d6ef
Pedro Gardim <me@pedrogardim.com>
2023-11-02 09:47:20 +0100

    feat: add tattoowork tattooist endpoints

--- src/controllers/tattooWorksControllers.ts
+++ src/controllers/tattooWorksControllers.ts
@@ -64,3 +64,65 @@ export const deleteTattooWork: Handler = async (req, res) => {
     throw { code: 404, message: "Tattoo work not found" };
   res.status(204).json(tattooWorkDeleted);
 };
+
+//User endpoints
+
+export const getMyTattooWorkById: Handler = async (req, res) => {
+  const tattooWork = await TattooWork.findOne({
+    relations: ["tattooist"],
+    where: { id: parseInt(req.params.id) },
+  });
+  if (!tattooWork) throw { code: 404, message: "Tatto work not found" };
+  res.status(200).json({ data: formatTattooWork(tattooWork, req) });
+};
+
+export const getMyTattooWorks: Handler = async (req, res) => {
+  let { pageSize = 10, page = 1 } = req.query;
+  pageSize = parseInt(pageSize as string);
+  page = parseInt(page as string);
+
+  const [tattooWorks, totalItems] = await TattooWork.findAndCount({
+    relations: ["tattooist"],
+    where: req.body,
+    take: pageSize,
+    skip: (page - 1) * pageSize,
+  });
+
+  res.status(200).json(
+    formatPaginationResponse({
+      req,
+      page,
+      pageSize,
+      totalItems,
+      items: tattooWorks.map((tattooWork) => formatTattooWork(tattooWork, req)),
+    })
+  );
+};
+
+export const createMyTattooWork: Handler = async (req, res) => {
+  validateTattooWorkData(req.body);
+  const createdTattooWork = await TattooWork.create(req.body).save();
+  res.status(201).json({ data: formatTattooWork(createdTattooWork, req) });
+};
+
+export const updateMyTattooWork: Handler = async (req, res) => {
+  validateTattooWorkData(req.body, true);
+  const tattooWorkRepository = AppDataSource.getRepository(TattooWork);
+  let tattooWork = await tattooWorkRepository.findOneBy({
+    id: parseInt(req.params.id),
+  });
+  if (!tattooWork) throw { code: 404, message: "Tattoo work not found" };
+  tattooWork = { ...tattooWork, ...req.body };
+  res
+    .status(200)
+    .json({ data: formatTattooWork(tattooWork as TattooWork, req) });
+};
+
+export const deleteMyTattooWork: Handler = async (req, res) => {
+  const tattooWorkDeleted = await TattooWork.delete({
+    id: parseInt(req.params.id),
+  });
+  if (!tattooWorkDeleted.affected)
+    throw { code: 404, message: "Tattoo work not found" };
+  res.status(204).json(tattooWorkDeleted);
+};
--- src/routes/tattooWorksRoutes.ts
+++ src/routes/tattooWorksRoutes.ts
@@ -5,11 +5,28 @@ import {
   createTattooWork,
   updateTattooWork,
   deleteTattooWork,
+  getMyTattooWorks,
+  getMyTattooWorkById,
+  createMyTattooWork,
+  updateMyTattooWork,
+  deleteMyTattooWork,
 } from "../controllers/tattooWorksControllers";
 import { asyncWrapper } from "../utils/wrappers";
 import { roleCheck } from "../middleware/roleCheck";
 
 const router = express.Router();
+
+//User - Tattooist CRUD
+
+const tattooistRouter = express.Router();
+tattooistRouter.use(roleCheck("tattooist"));
+//TODO
+
+tattooistRouter.get("/my/", asyncWrapper(getMyTattooWorks));
+tattooistRouter.get("/my/:id", asyncWrapper(getMyTattooWorkById));
+tattooistRouter.post("/my/", asyncWrapper(createMyTattooWork));
+tattooistRouter.put("/my/:id", asyncWrapper(updateMyTattooWork));
+tattooistRouter.delete("/my/:id", asyncWrapper(deleteMyTattooWork));
 
 //Admin CRUD
 const adminRouter = express.Router();
@@ -20,9 +37,6 @@ adminRouter.get("/:id", asyncWrapper(getTattooWorkById));
 adminRouter.post("/", asyncWrapper(createTattooWork));
 adminRouter.put("/:id", asyncWrapper(updateTattooWork));
 adminRouter.delete("/:id", asyncWrapper(deleteTattooWork));
-
-//User
-//TODO
 
 router.use("/", adminRouter);
 
